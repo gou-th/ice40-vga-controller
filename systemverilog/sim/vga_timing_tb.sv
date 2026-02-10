38b7a97e8aa0bf846513f8381c5a44a63148ce26
Goutham Ganesh Kumar <87598822+gou-th@users.noreply.github.com>
2026-02-10 23:04:59 +0530

    Updated README

--- systemverilog/sim/vga_timing_tb.sv
+++ systemverilog/sim/vga_timing_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01.12.2025 20:00:26
-// Design Name: 
-// Module Name: vga_timing_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module vga_timing_tb;
     
